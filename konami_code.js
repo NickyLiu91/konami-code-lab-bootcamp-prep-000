d6f91cc7f193aa3a83df1db3ba6c5bccbb8d7f4a
Nicky Liu <nickyliu91@gmail.com>
2018-06-09 11:16:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,48 +1,23 @@
-// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-// var index = 0
-
-// const body = document.body
-
-// function init() {
-//   body.addEventListener('keydown', function(e) {
-//     const key = parseInt(e.detail || e.which)
-
-//     if (key === code[index]) {
-
-//       if (index === code.length - 1) {
-//         alert("YOU DID IT!")
-//         index = 0
-//       } else {
-//         index++
-//       }
-//     } else {
-//       index = 0
-//     }
-//   })
-// }
-
-
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0
 
+const body = document.body
+
 function init() {
-  document.body.addEventListener('keydown', function(e){
-    const key = e.key
-    
-    if (key === const[index].key)
-    
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+
+      if (index === code.length - 1) {
+        alert("YOU DID IT!")
+        index = 0
+      } else {
+        index++
+      }
+    } else {
+      index = 0
+    }
   })
 }
